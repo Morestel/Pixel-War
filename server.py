3d0b288e7a2d91eb319fb0c8fc6f77bc632166c1
Morestel <mystkun6@gmail.com>
2022-04-05 23:05:25 +0200

    Building of the database in the server side

--- server.py
+++ server.py
@@ -2,6 +2,45 @@ from fastapi import Request, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pusher
 
+import sqlite3
+
+
+
+def setup_database(): # We build the database
+    conn = sqlite3.connect('pixel_base.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pixel(
+        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        x INTEGER,
+        y INTERGER,
+        color VARCHAR(7)
+    )
+    """)
+    conn.commit()
+    conn.close()
+    
+def insert_color(x, y, color): # We insert the color in the database
+    conn = sqlite3.connect('pixel_base.db')
+    cursor = conn.cursor()
+    cursor.execute("""SELECT x, y, color FROM pixel WHERE x = ? AND y = ?;""", (x, y))
+    pixel = cursor.fetchone()
+    # We start to check if the pixel are not already in the database
+    print(pixel)
+    if pixel == None: # It doesn't exist, we insert it
+        cursor.execute("""
+         INSERT INTO pixel(x, y, color) VALUES (?, ?, ?)
+         """, (x, y, color))
+        print('Inserted')
+    else: # It exists, we update it
+        cursor.execute("""
+                      UPDATE pixel SET color = ? WHERE x = ? AND y = ?; 
+                       """, (color, x, y))
+        print('Updated')
+    
+    conn.commit()
+    conn.close()
+    
 app = FastAPI()
 
 origins = [
